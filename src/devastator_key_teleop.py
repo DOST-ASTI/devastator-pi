ce8c3d0810ecfd41c397c1ab17657bf06e16b369
Joshua Javier <joshua.javier.alpha@gmail.com>
2019-05-23 15:46:55 +0800

    Renamed key_teleop file

--- src/devastator_key_teleop.py
+++ src/devastator_key_teleop.py
@@ -2,13 +2,16 @@
 
 from __future__ import print_function
 
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
-import rospy
-
-from geometry_msgs.msg import Twist
+import tty
+import termios
+import select
+import sys
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+import rospy
+import roslib
+roslib.load_manifest('teleop_twist_keyboard')
 
-import sys, select, termios, tty
 
 msg = """
 Reading from the keyboard  and Publishing to Twist!
@@ -26,25 +29,26 @@ CTRL-C to quit
 """
 
 moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
-    }
+    'i': (1, 0, 0, 0),
+    'o': (1, 0, 0, -1),
+    'j': (0, 0, 0, 1),
+    'l': (0, 0, 0, -1),
+    'u': (1, 0, 0, 1),
+    ',': (-1, 0, 0, 0),
+    '.': (-1, 0, 0, 1),
+    'm': (-1, 0, 0, -1),
+    'O': (1, -1, 0, 0),
+    'I': (1, 0, 0, 0),
+    'J': (0, 1, 0, 0),
+    'L': (0, -1, 0, 0),
+    'U': (1, 1, 0, 0),
+    '<': (-1, 0, 0, 0),
+    '>': (-1, -1, 0, 0),
+    'M': (-1, 1, 0, 0),
+    't': (0, 0, 1, 0),
+    'b': (0, 0, -1, 0),
+}
+
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -53,11 +57,12 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
 
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-    pub_key = rospy.Publisher('key', String, queue_size = 1)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    pub_key = rospy.Publisher('key', String, queue_size=1)
     rospy.init_node('teleop_twist_keyboard')
 
     speed = rospy.get_param("~speed", 0.5)
@@ -86,8 +91,12 @@ if __name__=="__main__":
                     break
 
             twist = Twist()
-            twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
-            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
+            twist.linear.x = x * speed
+            twist.linear.y = y * speed
+            twist.linear.z = z * speed
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = th * turn
             pub.publish(twist)
             pub_key.publish(key)
 
@@ -96,7 +105,11 @@ if __name__=="__main__":
 
     finally:
         twist = Twist()
-        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
         pub.publish(twist)
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
