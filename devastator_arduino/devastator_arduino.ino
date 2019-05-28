4533af253a207a73eaed231b0a4d7c26110972c2
Joshua Javier <joshua.javier.github@gmail.com>
2019-05-28 10:50:12 +0800

    Increased turn delay

--- devastator_arduino/devastator_arduino.ino
+++ devastator_arduino/devastator_arduino.ino
@@ -302,6 +302,9 @@ void loop()
   float sonic_F_range;
   float sonic_D_range;
 
+  string currentMove;
+  string previousMove;
+
   // Range limits
   float sonic_FF_zone = 0.20;
   float sonic_FD_zone = 0.20;
@@ -317,7 +320,9 @@ void loop()
     if (aIR_FR_range > aIR_FR_zone && aIR_FL_range > aIR_FL_zone){
       assess_front(sonic_F_range, sonic_D_range, dIR_F_range);
       while (sonic_F_range > sonic_FF_zone && (sonic_D_range > sonic_FD_zone)){
+        currentMove = 'forward';
         advance(motor_speed,motor_speed);
+      
       }
       assess_back(dIR_BR_range, dIR_BL_range);
       if (dIR_BR_range && dIR_BL_range)
@@ -327,11 +332,11 @@ void loop()
         assess_LR(aIR_FR_range, aIR_FL_range);
         if (aIR_FR_range > aIR_FL_range){
           turn_R(slow_motor_speed, slow_motor_speed);
-          delay(400);
+          delay(600);
         }
         else if (aIR_FL_range > aIR_FR_range){
           turn_L(slow_motor_speed, slow_motor_speed);
-          delay(400);
+          delay(600);
         }
         else if (aIR_FR_range < aIR_FR_zone && aIR_FL_range < aIR_FL_zone){
           turn_around(motor_speed, motor_speed);
@@ -368,11 +373,11 @@ void loop()
           assess_LR(aIR_FR_range, aIR_FL_range);
           if (aIR_FR_range > aIR_FL_range){
             turn_R(slow_motor_speed,slow_motor_speed);
-            delay(400);
+            delay(600);
           }
           else if (aIR_FL_range > aIR_FR_range){
             turn_L(slow_motor_speed, slow_motor_speed);
-            delay(400);
+            delay(600);
           }
           else if (aIR_FR_range < aIR_FR_zone && aIR_FL_range < aIR_FL_zone){
             turn_around(motor_speed,motor_speed);
